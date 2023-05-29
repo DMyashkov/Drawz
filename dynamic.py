2c478d369c58c8bac98c4633ce7737ba3378a57e
DMyashkov <58653406+DMyashkov@users.noreply.github.com>
2023-05-29 21:36:57 +0300

    model update

--- dynamic.py
+++ dynamic.py
@@ -1,10 +1,3 @@
 from os import walk, getcwd
 
-mypath = "data/"
-txt_name_list = []
-for (dirpath, dirnames, filenames) in walk(mypath):
-    if filenames != '.DS_Store':       ##Ugh mac junk
-        txt_name_list.extend(filenames)
-        break
-
-print(txt_name_list)
+wantedObjects = {"anvil", "cookie", "eyeglasses"}
